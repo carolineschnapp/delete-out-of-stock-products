c133785e28a74fcb1aeeadc4629f434d6cac870a
Caroline Schnapp <mllegeorgesand@gmail.com>
2010-10-05 04:57:12 -0400

    use a 1.upto(nb_pages) construct for engine

--- cleanup.rb
+++ cleanup.rb
@@ -1,54 +1,50 @@
+require 'rubygems'
+require 'shopify_api'
+
 APIKEY = 'APIKEY'
 PASSWORD = 'PASSWORD'
 SHOPNAME = 'shopname'
 
-CYCLE = 10 * 60       # 10 minutes times 60 seconds per minute. Total in seconds.
-
-require 'rubygems'    # Need this to use the shopify_api gem.
-require 'shopify_api' # shopify_api gem is tellement utile to speak to your shop.
+CYCLE = 10 * 60
 
 # Telling your shop who's boss.
 ShopifyAPI::Base.site = "http://#{APIKEY}:#{PASSWORD}@#{SHOPNAME}.myshopify.com/admin"
 
 # Initializing.
-page = 1
-start_watch = Time.now
+start_time = Time.now
 
 # How many.
-count = ShopifyAPI::Product.count
-nb_pages = (count/250.0).ceil
+product_count = ShopifyAPI::Product.count
+nb_pages      = (product_count / 250.0).ceil
 
 # Do we actually have any work to do?
-if nb_pages.zero?
-  puts "Yo man. You don't have any product in your shop."
-end
+puts "Yo man. You don't have any product in your shop. duh!" if product_count.zero?
 
 # While we still have products.
-while (page <= nb_pages) do
+1.upto(nb_pages) do |page|
   unless page == 1
-    stop_watch = Time.now
-    puts "Our last batch processing started at #{start_watch.strftime('%I:%M%p')}"
-    puts "The time is now #{stop_watch.strftime('%I:%M%p')}"
-    duration = stop_watch - start_watch
-    puts "The processing lasted #{duration} seconds."
-    wait_time = CYCLE - duration
-    puts "We have to wait #{wait_time} seconds then we will resume."
+    stop_time = Time.now
+    puts "Current batch processing started at #{start_time.strftime('%I:%M%p')}"
+    puts "The time is now #{stop_time.strftime('%I:%M%p')}"
+    processing_duration = stop_time - start_time
+    puts "The processing lasted #{processing_duration.to_i} seconds."
+    wait_time = CYCLE - processing_duration
+    puts "We have to wait #{wait_time.to_i} seconds then we will resume."
     sleep wait_time
-    start_watch = Time.now
+    start_time = Time.now
   end
-  puts "Doing page #{page} of #{nb_pages}..."
+  puts "Doing page #{page}/#{nb_pages}..."
   products = ShopifyAPI::Product.find( :all, :params => { :limit => 250, :page => page } )
   products.each do |product|
     puts product.title
-    variants = product.variants.select do |variant|
+    any_in_stock = product.variants.any? do |variant|
       variant.inventory_management == '' || variant.inventory_policy == 'continue' || variant.inventory_quantity > 0
     end
-    if variants.empty?
+    if not any_in_stock
       puts "--- Deleting #{product.title}..."
       product.destroy
     end
   end
-  page += 1
 end
 
 puts "Over and out."
